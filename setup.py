346273152089cf652c712bea8c0fabbf915c27c1
mt_licit <michele.tirico@entpe.fr>
2023-07-13 11:07:17 +0200

    test setup

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='logger',
-    version='0.0.1',
+    version='0.0.2',
     author='Michele Tirico',
    # author_mail="tirico.michele@outlook.com",
     description='glogger',
